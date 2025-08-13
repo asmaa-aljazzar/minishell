d5580997be0e5c380fd138baa94786e5aae54e50
basma-almohamad <almohamad.basma.32@gmail.com>
2025-08-13 17:55:02 +0000

    return missing functions

--- srcs/prog/parsing/allocate_commands.c
+++ srcs/prog/parsing/allocate_commands.c
@@ -7,36 +7,23 @@ t_command *create_command_list(t_minishell *ms, int count)
     t_command *curr = NULL;
     t_command *new;
     int i = 0;
-}
 
-void	allocate_commands(t_minishell *ms) //todo 25 line norm error
-{
-	t_command	*head;
-	t_command	*curr;
-	int			count;
-	int			i;
-	t_command	*new;
-
-	head = NULL;
-	curr = NULL;
-	count = ms->pipe_count;
-	i = 0;
-	while (i <= count)
-	{
-		new = init_command(ms);
-		if (!head)
-		{
-			head = new;
-			curr = new;
-		}
-		else
-		{
-			curr->next = new;
-			curr = new;
-		}
-		i++;
-	}
-	ms->cmd = head;
+    while (i <= count)
+    {
+        new = init_command(ms);
+        if (!head)
+        {
+            head = new;
+            curr = new;
+        }
+        else
+        {
+            curr->next = new;
+            curr = new;
+        }
+        i++;
+    }
+    return head;
 }
 
 // void allocate_commands(t_minishell *ms) this function adds extra step only
